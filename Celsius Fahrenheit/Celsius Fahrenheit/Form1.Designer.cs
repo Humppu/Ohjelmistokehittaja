00b3b31ccf86626599ef45b6fead1489ece992c8
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2024-01-18 15:07:37 +0200

    Valmis

---
+++ Celsius Fahrenheit/Celsius Fahrenheit/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Celsius_Fahrenheit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CelsiusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna asteet: ";
+            // 
+            // CelsiusRB
+            // 
+            this.CelsiusRB.AutoSize = true;
+            this.CelsiusRB.Location = new System.Drawing.Point(9, 29);
+            this.CelsiusRB.Name = "CelsiusRB";
+            this.CelsiusRB.Size = new System.Drawing.Size(58, 17);
+            this.CelsiusRB.TabIndex = 2;
+            this.CelsiusRB.TabStop = true;
+            this.CelsiusRB.Text = "Celsius";
+            this.CelsiusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Location = new System.Drawing.Point(9, 64);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(75, 17);
+            this.FahrenheitRB.TabIndex = 3;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AsteetTB.Location = new System.Drawing.Point(114, 65);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(32, 20);
+            this.AsteetTB.TabIndex = 4;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(37, 122);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(45, 13);
+            this.VastausLB.TabIndex = 5;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.MuunnaBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MuunnaBT.Location = new System.Drawing.Point(312, 65);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(75, 23);
+            this.MuunnaBT.TabIndex = 6;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = false;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FahrenheitRB);
+            this.groupBox1.Controls.Add(this.CelsiusRB);
+            this.groupBox1.Location = new System.Drawing.Point(175, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(103, 85);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Miten Muunnat? ";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(394, 144);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Celsius Fahrenheit";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton CelsiusRB;
+        private System.Windows.Forms.RadioButton FahrenheitRB;
+        private System.Windows.Forms.TextBox AsteetTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button MuunnaBT;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
