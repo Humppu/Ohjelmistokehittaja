66d402d3b682eacb16873453344736b7a86b75a9
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2024-01-18 17:16:03 +0200

    Valmis

---
+++ BMI-laskuri/BMI-laskuri/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BMI_laskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PainoTB = new System.Windows.Forms.TextBox();
+            this.PituusTB = new System.Windows.Forms.TextBox();
+            this.BMIBT = new System.Windows.Forms.Button();
+            this.BmiLB = new System.Windows.Forms.Label();
+            this.KuvausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna Paino: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna pituus: ";
+            // 
+            // PainoTB
+            // 
+            this.PainoTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PainoTB.Location = new System.Drawing.Point(115, 35);
+            this.PainoTB.Name = "PainoTB";
+            this.PainoTB.Size = new System.Drawing.Size(69, 20);
+            this.PainoTB.TabIndex = 2;
+            // 
+            // PituusTB
+            // 
+            this.PituusTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PituusTB.Location = new System.Drawing.Point(115, 74);
+            this.PituusTB.Name = "PituusTB";
+            this.PituusTB.Size = new System.Drawing.Size(69, 20);
+            this.PituusTB.TabIndex = 3;
+            // 
+            // BMIBT
+            // 
+            this.BMIBT.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BMIBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BMIBT.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BMIBT.Location = new System.Drawing.Point(58, 117);
+            this.BMIBT.Name = "BMIBT";
+            this.BMIBT.Size = new System.Drawing.Size(147, 23);
+            this.BMIBT.TabIndex = 4;
+            this.BMIBT.Text = "Laske painoindeksi";
+            this.BMIBT.UseVisualStyleBackColor = false;
+            this.BMIBT.Click += new System.EventHandler(this.BMIBT_Click);
+            // 
+            // BmiLB
+            // 
+            this.BmiLB.AutoSize = true;
+            this.BmiLB.Location = new System.Drawing.Point(31, 171);
+            this.BmiLB.Name = "BmiLB";
+            this.BmiLB.Size = new System.Drawing.Size(45, 13);
+            this.BmiLB.TabIndex = 5;
+            this.BmiLB.Text = "Vastaus";
+            this.BmiLB.Visible = false;
+            // 
+            // KuvausLB
+            // 
+            this.KuvausLB.AutoSize = true;
+            this.KuvausLB.Location = new System.Drawing.Point(31, 197);
+            this.KuvausLB.Name = "KuvausLB";
+            this.KuvausLB.Size = new System.Drawing.Size(43, 13);
+            this.KuvausLB.TabIndex = 6;
+            this.KuvausLB.Text = "Kuvaus";
+            this.KuvausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(259, 234);
+            this.Controls.Add(this.KuvausLB);
+            this.Controls.Add(this.BmiLB);
+            this.Controls.Add(this.BMIBT);
+            this.Controls.Add(this.PituusTB);
+            this.Controls.Add(this.PainoTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "BMI-laskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PainoTB;
+        private System.Windows.Forms.TextBox PituusTB;
+        private System.Windows.Forms.Button BMIBT;
+        private System.Windows.Forms.Label BmiLB;
+        private System.Windows.Forms.Label KuvausLB;
+    }
+}
+
