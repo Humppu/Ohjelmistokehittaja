b4cd7cc603463d611597209813ffd649d70ecd12
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2024-01-29 13:12:12 +0200

    Valmis

---
+++ Sekuntikello/Sekuntikello/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Sekkari
+{
+    partial class sekkariFM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kelloLB = new System.Windows.Forms.Label();
+            this.kaynnistaBT = new System.Windows.Forms.Button();
+            this.pysaytaBT = new System.Windows.Forms.Button();
+            this.nollaaBT = new System.Windows.Forms.Button();
+            this.ajastinTM = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // kelloLB
+            // 
+            this.kelloLB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kelloLB.Location = new System.Drawing.Point(97, 33);
+            this.kelloLB.Name = "kelloLB";
+            this.kelloLB.Size = new System.Drawing.Size(460, 145);
+            this.kelloLB.TabIndex = 0;
+            this.kelloLB.Visible = false;
+            // 
+            // kaynnistaBT
+            // 
+            this.kaynnistaBT.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.kaynnistaBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kaynnistaBT.Location = new System.Drawing.Point(97, 213);
+            this.kaynnistaBT.Name = "kaynnistaBT";
+            this.kaynnistaBT.Size = new System.Drawing.Size(75, 23);
+            this.kaynnistaBT.TabIndex = 1;
+            this.kaynnistaBT.Text = "Start";
+            this.kaynnistaBT.UseVisualStyleBackColor = false;
+            this.kaynnistaBT.Click += new System.EventHandler(this.kaynnistaBT_Click);
+            // 
+            // pysaytaBT
+            // 
+            this.pysaytaBT.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pysaytaBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pysaytaBT.Location = new System.Drawing.Point(297, 213);
+            this.pysaytaBT.Name = "pysaytaBT";
+            this.pysaytaBT.Size = new System.Drawing.Size(75, 23);
+            this.pysaytaBT.TabIndex = 2;
+            this.pysaytaBT.Text = "Stop";
+            this.pysaytaBT.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.pysaytaBT.UseVisualStyleBackColor = false;
+            this.pysaytaBT.Click += new System.EventHandler(this.pysaytaBT_Click_1);
+            // 
+            // nollaaBT
+            // 
+            this.nollaaBT.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.nollaaBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nollaaBT.Location = new System.Drawing.Point(482, 213);
+            this.nollaaBT.Name = "nollaaBT";
+            this.nollaaBT.Size = new System.Drawing.Size(75, 23);
+            this.nollaaBT.TabIndex = 3;
+            this.nollaaBT.Text = "Reset";
+            this.nollaaBT.UseVisualStyleBackColor = false;
+            this.nollaaBT.Click += new System.EventHandler(this.nollaaBT_Click);
+            // 
+            // ajastinTM
+            // 
+            this.ajastinTM.Enabled = true;
+            // 
+            // sekkariFM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(657, 322);
+            this.Controls.Add(this.kelloLB);
+            this.Controls.Add(this.nollaaBT);
+            this.Controls.Add(this.pysaytaBT);
+            this.Controls.Add(this.kaynnistaBT);
+            this.Name = "sekkariFM";
+            this.Text = "Stopwatch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label kelloLB;
+        private System.Windows.Forms.Button kaynnistaBT;
+        private System.Windows.Forms.Button pysaytaBT;
+        private System.Windows.Forms.Button nollaaBT;
+        private System.Windows.Forms.Timer ajastinTM;
+    }
+}
+
---
+++ Sekuntikello/Sekuntikello/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sekkari
+{
+    public partial class sekkariFM : Form
+    {
+        private Stopwatch sekkari = new Stopwatch();
+
+
+        public sekkariFM()
+        {
+            InitializeComponent();
+        }
+
+
+        private void kaynnistaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Start(); 
+        }
+
+        private void pysaytaBT_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nollaaBT_Click(object sender, EventArgs e)
+        {
+            sekkari.Reset();
+        }
+
+
+        private void ajastinTM_Tick(object sender, EventArgs e)
+        {
+            kelloLB.Text = String.Format("{00:hh\\:mm\\:ss\\:fff}", sekkari.Elapsed);
+        }
+    }
+}
