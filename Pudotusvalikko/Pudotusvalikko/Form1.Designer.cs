1a928b78b9ebcf91ff623ab2c462dda9aee1bc8c
Homam Al-Mehsen <homam.al-mehsen@edu.keuda.fi>
2024-01-30 12:08:28 +0200

    Kesken

---
+++ Pudotusvalikko/Pudotusvalikko/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Pudotusvalikko
+{
+    partial class OppilaitosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VastuuhenkiloCB = new System.Windows.Forms.ComboBox();
+            this.OppilaitoksetCB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oppilaitosten avainhenkilöt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valitse oppilaitos: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(241, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valitse vastuuhenkilö:";
+            // 
+            // VastuuhenkiloCB
+            // 
+            this.VastuuhenkiloCB.FormattingEnabled = true;
+            this.VastuuhenkiloCB.Location = new System.Drawing.Point(245, 128);
+            this.VastuuhenkiloCB.Name = "VastuuhenkiloCB";
+            this.VastuuhenkiloCB.Size = new System.Drawing.Size(168, 33);
+            this.VastuuhenkiloCB.TabIndex = 3;
+            // 
+            // OppilaitoksetCB
+            // 
+            this.OppilaitoksetCB.FormattingEnabled = true;
+            this.OppilaitoksetCB.Location = new System.Drawing.Point(63, 128);
+            this.OppilaitoksetCB.Name = "OppilaitoksetCB";
+            this.OppilaitoksetCB.Size = new System.Drawing.Size(130, 33);
+            this.OppilaitoksetCB.TabIndex = 4;
+            this.OppilaitoksetCB.SelectedIndexChanged += new System.EventHandler(this.OppilaitoksetCB_SelectedIndexChanged);
+            // 
+            // OppilaitosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(772, 569);
+            this.Controls.Add(this.OppilaitoksetCB);
+            this.Controls.Add(this.VastuuhenkiloCB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "OppilaitosForm";
+            this.Text = "Oppilaitoksen avainhenkilöt";
+            this.Load += new System.EventHandler(this.OppilaitosForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox VastuuhenkiloCB;
+        private System.Windows.Forms.ComboBox OppilaitoksetCB;
+    }
+}
+
---
+++ Pudotusvalikko/Pudotusvalikko/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Pudotusvalikko
+{
+
+    DataTable oppilaitos = new DataTable();
+    DataTable vastuuhenkilot = new DataTable();
+    DataTable yhteys = new DataTable();
+
+
+
+    public partial class OppilaitosForm : Form
+    {
+        public OppilaitosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OppilaitosForm_Load(object sender, EventArgs e)
+        {
+            taytaOppilaitosTaulukko();
+            OppilaitoksetCB.DataSource = oppilaitos;
+            OppilaitoksetCB.DisplayMember = "Onimi";
+            taytaVastuuHenkilotTaulukko(); 
+        }
+
+        private void OppilaitoksetCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string viite = oppilaitos.Rows[OppilaitoksetCB.SelectedIndex]["OID"].ToString();
+
+        }
+    }
+}
